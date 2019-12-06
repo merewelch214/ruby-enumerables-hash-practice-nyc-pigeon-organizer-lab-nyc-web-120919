356f9cbb70ba6d1992956d14711f5c38ad2bd796
Meredith Welch <merewelch@gmail.com>
2019-12-06 13:49:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  final_hash = {}
+  new_arr = []
+  data.each do |color, purple_hash|
+    purple_hash.each do |col, names|
+      names.each do |pigs|
+        if final_hash.has_key?(pigs)
+          if final_hash[pigs].has_key?(color) 
+            final_hash[pigs][color] << col.to_s
+          else 
+            new_arr = []
+            new_arr << col.to_s
+            final_hash[pigs][color] = new_arr
+            new_arr = []
+          end
+        else 
+          new_arr << col.to_s
+          final_hash[pigs] = {color => new_arr}
+          new_arr = []
+        end
+      end
+    end
+  end
+  p final_hash
+  return final_hash
+end  
